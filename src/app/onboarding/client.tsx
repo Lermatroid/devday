4858ce1caeac7e7822a1d17ca1ce191b8d660cc6
Liam Murray <liam@murray.to>
2025-05-02 14:58:20 -0500

    theme woo hoo!!!!!!!!

--- src/app/onboarding/client.tsx
+++ src/app/onboarding/client.tsx
@@ -10,9 +10,14 @@ import {
 	SelectValue,
 } from "@/components/ui/select";
 
+import { setCookie, getCookie } from "cookies-next/client";
+
 export function ThemeSwitcher() {
 	return (
-		<Select>
+		<Select
+			defaultValue={getCookie("theme") ?? "light"}
+			onValueChange={(value) => setCookie("theme", value)}
+		>
 			<SelectTrigger className="w-[180px]">
 				<SelectValue placeholder="Select a theme" />
 			</SelectTrigger>
